409a04f8a0a085e48a2833aec1e7404cc22f99ed
Zen_99 <2019cs161@stu.ucsc.lk>
2022-09-21 05:43:12 +0530

    home page and navigator

---
+++ src/screens/SchoolVan.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const SchoolVan = () => {
+  return (
+    <View>
+      <Text>SchoolVan</Text>
+    </View>
+  );
+};
+
+export default SchoolVan;
+
+const styles = StyleSheet.create({});

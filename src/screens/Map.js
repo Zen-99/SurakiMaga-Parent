cd50ce2f9ced7b1fa9bb8771a737750d89cc8d19
Zen_99 <2019cs161@stu.ucsc.lk>
2022-10-22 14:25:52 +0530

    School van screen

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,14 +1,244 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ActivityIndicator,
+  Image,
+  Modal,
+} from "react-native";
+import MapViewDirections from "react-native-maps-directions";
+import { useIsFocused } from "@react-navigation/native";
+import MapView, { Callout, Marker } from "react-native-maps";
+import { colors, parameters, mapStyle } from "../globals/styles";
+import apiClient from "../Services/apiClient";
+import React, { useEffect, useState, useLayoutEffect } from "react";
+import * as Location from "expo-location";
 
 const Map = () => {
+  const isFocused = useIsFocused();
+  const [position, setPosition] = useState({
+    accuracy: 11.454999923706055,
+    altitude: -89.19999694824219,
+    altitudeAccuracy: 1.509387731552124,
+    heading: 270.5041198730469,
+    latitude: 6.93371,
+    longitude: 79.85548,
+    speed: 0.0023679917212575674,
+  });
+  const destination = {
+    latitude: 6.909044420770567,
+    longitude: 79.87515656700285,
+  };
+  const origin = { latitude: position.latitude, longitude: position.longitude };
   return (
-    <View>
-      <Text>Map</Text>
+    <View style={styles.container}>
+      <View style={styles.upperBox}>
+        <View style={styles.firstRow}>
+          <Text style={styles.firstRowFont}>16 KM Left</Text>
+        </View>
+        <View style={styles.secondRow}>
+          <View style={styles.left}>
+            <Text style={styles.secondRowFont}>Total Distance</Text>
+          </View>
+          <View style={styles.right}>
+            <Text style={styles.secondRowFont}>16 KM</Text>
+          </View>
+        </View>
+        <View style={styles.secondRow}>
+          <View style={styles.left}>
+            <Text style={styles.secondRowFont}>Current Speed</Text>
+          </View>
+          <View style={styles.right}>
+            <Text style={styles.secondRowFont}>0Kmph</Text>
+          </View>
+        </View>
+      </View>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 6.902333307966652,
+          longitude: 79.8611743548024,
+          latitudeDelta: 0.007,
+          longitudeDelta: 0.008,
+        }}
+        showsUserLocation={true}
+        // customMapStyle={mapStyle}
+        followsUserLocation={true}
+      >
+        <Marker
+          coordinate={{
+            latitude: 6.909044420770567,
+            longitude: 79.87515656700285,
+          }}
+          pinColor="green"
+        >
+          <Image
+            style={{ width: 40, height: 40, resizeMode: "cover" }}
+            source={require("../../assets/images/schoolicon.png")}
+          />
+        </Marker>
+      </MapView>
     </View>
   );
 };
 
 export default Map;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: parameters.SCREEN_WIDTH,
+    height: parameters.SCREEN_HEIGHT,
+  },
+  loading: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  upperBox: {
+    position: "absolute",
+    display: "flex",
+    top: 30,
+    alignSelf: "center",
+    alignItems: "center",
+    elevation: 10,
+    zIndex: 1000,
+    backgroundColor: colors.midBoxWhite,
+    borderRadius: 15,
+    width: (parameters.SCREEN_WIDTH * 7) / 8,
+    height: (parameters.SCREEN_HEIGHT * 1) / 5,
+    shadowColor: "#7F5DF0",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+  },
+  firstRow: {
+    display: "flex",
+    width: "100%",
+    height: "40%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  secondRow: {
+    display: "flex",
+    width: "80%",
+    flexDirection: "row",
+    alignItems: "center",
+    height: "15%",
+  },
+  left: {
+    display: "flex",
+    width: "70%",
+    height: "100%",
+
+    justifyContent: "center",
+  },
+  right: {
+    width: "30%",
+    height: "100%",
+    justifyContent: "center",
+  },
+  firstRowFont: {
+    color: colors.orange,
+    fontWeight: "bold",
+    fontSize: 45,
+  },
+  secondRowFont: {
+    color: colors.font,
+    fontSize: 20,
+  },
+  alert: {
+    flex: 1,
+    backgroundColor: "#00000090",
+    alignItems: "center",
+    justifyContent: "center",
+    width: parameters.SCREEN_WIDTH,
+    height: parameters.SCREEN_HEIGHT,
+
+    // backgroundColor:'red',
+  },
+  alertbox: {
+    paddingTop: 5,
+    display: "flex",
+    borderRadius: 5,
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    width: (parameters.SCREEN_WIDTH * 4) / 5,
+    height: (parameters.SCREEN_HEIGHT * 5) / 20,
+    backgroundColor: colors.midBoxWhite,
+    // shadowColor: '#171717',
+    // shadowOffset: {width: -3, height: 4},
+    // shadowOpacity: 1,
+    // shadowRadius: 3,
+  },
+  alertName: {
+    fontSize: 20,
+  },
+  alertSchool: {
+    fontSize: 15,
+  },
+  confirmbtn: {
+    display: "flex",
+    flexDirection: "row",
+    height: 40,
+    width: parameters.SCREEN_WIDTH / 4,
+    backgroundColor: colors.orange,
+    borderRadius: 5,
+    alignItems: "center",
+    alignSelf: "center",
+    justifyContent: "center",
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  confirmbtnText: {
+    alignSelf: "center",
+    justifyContent: "center",
+    color: colors.white,
+    fontWeight: "bold",
+    fontSize: 16,
+    marginTop: -2,
+  },
+  imageName: {
+    display: "flex",
+    flexDirection: "row",
+    width: "80%",
+    height: "80%",
+    alignItems: "center",
+  },
+  image: {
+    width: "40%",
+    height: "100%",
+    justifyContent: "center",
+  },
+  name: {
+    width: "60%",
+    height: "50%",
+    justifyContent: "space-around",
+  },
+  picture: {
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+  },
+  buttonSet: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    width: "98%",
+  },
+});
--- src/screens/SchoolVan.js
+++ src/screens/SchoolVan.js
@@ -1,14 +1,313 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  StatusBar,
+} from "react-native";
+import { TextInput } from "react-native-paper";
+import apiClient from "../Services/apiClient";
+import { Icon } from "react-native-elements";
+import { colors, parameters } from "../globals/styles";
+import React, { useState, useEffect } from "react";
+import Slideshow from "react-native-image-slider-show";
+import Header from "../context/Header";
 
 const SchoolVan = () => {
+  let imageBucket = [];
+  const [information, setInformation] = useState({
+    owner: "",
+    Driver: "",
+    vehicleno: "",
+    DriverImage: "",
+  });
   return (
-    <View>
-      <Text>SchoolVan</Text>
+    <View style={styles.container}>
+      <Header />
+      <ScrollView
+        style={styles.scrollview}
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={styles.nameView}>
+          <Text style={styles.title}>School Van</Text>
+        </View>
+        <Slideshow
+          containerStyle={styles.slideshow}
+          overlay={true}
+          dataSource={imageBucket}
+        />
+        <View style={styles.nameBox2}>
+          <View style={styles.informationTopic}>
+            <Text style={styles.informationTopicText}>Information</Text>
+          </View>
+          <View style={styles.informationBody}>
+            <View style={styles.informationDetail}>
+              <Text style={styles.informationDetailText}>Vehicle No</Text>
+              <Text style={styles.informationDetailText}>
+                {information.vehicleno}
+              </Text>
+            </View>
+            <View style={styles.informationDetail}>
+              <Text style={styles.informationDetailText}>Owner</Text>
+              <Text style={styles.informationDetailText}>
+                {information.owner}
+              </Text>
+            </View>
+            <View style={styles.informationDetail}>
+              <Text style={styles.informationDetailText}>Driver</Text>
+              <Text style={styles.informationDetailText}>
+                {information.Driver}
+              </Text>
+            </View>
+          </View>
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text
+            style={styles.buttonText}
+            onPress={() => navigation.navigate("HomeScreen")}
+          >
+            Comlpaints & Reviews
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text
+            style={styles.buttonText}
+            onPress={() => navigation.navigate("HomeScreen")}
+          >
+            Payments
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button2}>
+          <Text style={styles.button2Text}>Leave This school van</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 };
 
 export default SchoolVan;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    backgroundColor: colors.white,
+    height: parameters.SCREEN_HEIGHT,
+    paddingTop: parameters.statusBarHeight,
+    alignItems: "center",
+  },
+  header: {
+    position: "absolute",
+    top: 30,
+    elevation: 10,
+    zIndex: 1000,
+    marginTop: parameters.statusBarHeight,
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: colors.midBoxWhite,
+    height: parameters.headerHeight,
+
+    borderRadius: 10,
+    alignItems: "center",
+    width: (parameters.SCREEN_WIDTH * 9) / 10,
+    justifyContent: "space-between",
+    shadowColor: "#7F5DF0",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+
+    // height:parameters.SCREEN_HEIGHT/7,
+  },
+  leftSideOfHeader: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    //   paddingLeft:parameters.SCREEN_WIDTH/20
+  },
+  rightSideOfHeader: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    paddingRight: parameters.SCREEN_WIDTH / 20,
+  },
+  icon2: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 80,
+    height: 100,
+  },
+  profilePicSmall: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+  logo: {
+    marginLeft: 20,
+    width: (parameters.SCREEN_WIDTH * 1) / 8,
+    height: (parameters.SCREEN_HEIGHT * 0.5) / 8,
+  },
+  scrollview: {
+    display: "flex",
+    marginBottom: 20,
+  },
+  slideshow: {
+    alignSelf: "center",
+    width: parameters.SCREEN_WIDTH,
+    height: (parameters.SCREEN_HEIGHT * 1) / 4,
+    borderRadius: 10,
+    backgroundColor: colors.orange,
+  },
+  nameView: {
+    marginTop: parameters.SCREEN_HEIGHT / 6,
+    display: "flex",
+    width: parameters.SCREEN_WIDTH,
+    height: parameters.SCREEN_HEIGHT / 8,
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: colors.font,
+    fontSize: 30,
+    fontFamily: "sans-serif-medium",
+    marginTop: -2,
+  },
+  nameBox2: {
+    display: "flex",
+    backgroundColor: colors.midBoxWhite,
+    alignSelf: "center",
+    width: (parameters.SCREEN_WIDTH * 11) / 12,
+    height: parameters.SCREEN_HEIGHT / 4,
+    marginBottom: "7%",
+    alignItems: "center",
+    marginTop: "7%",
+    borderRadius: 10,
+    shadowColor: "#7F5DF0",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+  },
+
+  informationTopic: {
+    display: "flex",
+    borderRadius: 10,
+    width: "85%",
+    height: "30%",
+    justifyContent: "center",
+    borderBottomColor: "grey",
+    borderBottomWidth: 2,
+  },
+  informationTopicText: {
+    color: colors.grey,
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  informationDetailText: {
+    color: colors.grey,
+    fontSize: 18,
+  },
+  informationDetailText2: {
+    color: colors.grey2,
+    fontSize: 18,
+  },
+  informationBody: {
+    display: "flex",
+    borderRadius: 10,
+    width: "100%",
+    height: "70%",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  informationDetail: {
+    display: "flex",
+    width: "85%",
+    paddingBottom: 5,
+    padddingTop: 5,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  informationDetail2: {
+    display: "flex",
+    width: "85%",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  button2: {
+    display: "flex",
+    flexDirection: "row",
+    height: 60,
+    width: (parameters.SCREEN_WIDTH * 4) / 6,
+    backgroundColor: "white",
+    borderRadius: 20,
+    alignSelf: "center",
+    alignItems: "center",
+    marginTop: 10,
+    justifyContent: "center",
+    marginBottom: 100,
+  },
+  button2Text: {
+    color: colors.orange,
+    fontSize: 22,
+    fontFamily: "sans-serif-medium",
+    marginTop: -2,
+  },
+  textInput: {
+    width: (parameters.SCREEN_WIDTH * 4) / 6,
+    height: 50,
+    marginBottom: 10,
+    backgroundColor: "white",
+  },
+  emergencyBody: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "70%",
+  },
+  button: {
+    height: 60,
+    width: (parameters.SCREEN_WIDTH * 4) / 6,
+    backgroundColor: colors.orange,
+    borderRadius: 20,
+    alignSelf: "center",
+    justifyContent: "center",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  buttonText: {
+    alignSelf: "center",
+    justifyContent: "center",
+    color: colors.white,
+    fontSize: 22,
+    marginTop: -2,
+  },
+  nameBox4: {
+    display: "flex",
+    backgroundColor: colors.midBoxWhite,
+    alignSelf: "center",
+    width: (parameters.SCREEN_WIDTH * 11) / 12,
+    height: parameters.SCREEN_HEIGHT / 3,
+    alignItems: "center",
+    marginTop: "7%",
+    borderRadius: 10,
+    shadowColor: "#7F5DF0",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+  },
+});
